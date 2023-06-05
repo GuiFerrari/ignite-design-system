569f2efe23892952f55f60bdfd46936ececea85e
Guilherme Ferrari <guiferrari10@hotmail.com>
2023-06-05 16:45:46 -0300

    build: created tokens on story book

--- packages/docs/.storybook/preview.js
+++ packages/docs/.storybook/preview.js
@@ -1,13 +1,16 @@
-/** @type { import('@storybook/react').Preview } */
+import { themes } from '@storybook/theming'
+
 const preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
-        color: /(background|color)$/i,
         date: /Date$/,
       },
     },
+    docs: {
+      theme: themes.dark
+    }
   },
 };
 
